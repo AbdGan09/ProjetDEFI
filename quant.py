483ec1b7c7abcaa0576e0b9331f44a54f103b6e5
Abdoul Bagagnan <Abdoul@contactharisfinancial.onmicrosoft.com>
2023-10-30 18:53:47 +0100

    definition des fonctions a developper

---
+++ quant.py
@@ -0,0 +1,33 @@
+#importation des fichier
+
+##methode à utiliser après
+
+
+#MarketZeroCouponCurve
+
+def MarketZeroCouponCurve(m):
+
+    return Pm
+
+#simulation de trajectoire
+
+
+#plot des trajectoire
+
+
+#fonction A(t,T)
+
+
+#fonction Theta
+
+
+#fonction B(t,  T)
+
+
+#fonction P(t,T)
+
+
+#Modele de Hull White
+
+
+#fonction Vrect
