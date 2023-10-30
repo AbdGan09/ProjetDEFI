02e04af439b894b89df96a239a7467cd2b22684c
Abdoul Bagagnan <Abdoul@contactharisfinancial.onmicrosoft.com>
2023-10-30 20:15:42 +0100

    organisation et définition des generalité

--- quant.py
+++ quant.py
@@ -1,33 +1,72 @@
 #importation des fichier
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy as sc
 
-##methode à utiliser après
+##methode à utiliser après###
+
+#definition de la class que monsieur a demander dans son cours:
+class MarketZeroCoupon:
+    def __init__(self, parametre):
+        self.parametre = parametre
+
+    @staticmethod
+    def getMarketZeroCouponCurve(parametre):
+        return marketZeroCouponCurve(parametre)
+
+    @staticmethod
+    def getmarketZeroCouponInstFwdCurve(parametre):
+        return marketZeroCouponInstFwdCurve(parametre)
+
+#comme discuté j'ai juste écrire les classes les fonctions à l'intérieur on se les repartis
+
+
+#si quelqu'un juge necessaire de faire des function suplémentaire il peux le faire l'objectif étant de mieu segmenté notre code pour le rendre plus lisible
+
+
 
 
 #MarketZeroCouponCurve
+def marketZeroCouponCurve():
+    pass
 
-def MarketZeroCouponCurve(m):
 
-    return Pm
+def marketZeroCouponInstFwdCurve():
+    pass
 
-#simulation de trajectoire
+
+#definition de la derivee Seconde a etre utilise après pour le calcul de Theta
+def getSecondDerive(param, fonction):
+    pass
 
 
 #plot des trajectoire
+def plotSimulation():
+    pass
 
 
 #fonction A(t,T)
+def A():
+    pass
 
 
 #fonction Theta
+def gettheta():
+    return Theta
 
 
-#fonction B(t,  T)
+#fonction B(t, T)
+def B():
+    pass
 
 
-#fonction P(t,T)
+#fonction P(t,T): The Zero Coupon price
+def zeroCoupon():
+    pass
 
 
 #Modele de Hull White
-
-
-#fonction Vrect
+def hullWhite():
+    pass
---
+++ unit.py
@@ -0,0 +1,12 @@
+#lieu pour mettre les class ou fonction de test:
+from pricing import *
+from quant import *
+
+class Main:
+    def __init__(self, param):
+        pass
+
+    def TestSimulation(self, param):
+        pass
+
+    def Test
