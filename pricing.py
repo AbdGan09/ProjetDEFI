1a9b4fd9c5af20c58cc0f5773d6e40048a916284
PierreL77 <129327699+PierreL77@users.noreply.github.com>
2023-11-08 19:37:07 +0100

    Update pricing.py

--- pricing.py
+++ pricing.py
@@ -8,9 +8,31 @@ def pricingSwap():
     pass
 
 
-#simulation de trajectoire
-def generateurTrajectoire():
-    pass
+#simulation de trajectoire d'un brownien
+def brownian(M,I):
+    def gen_sn(M, I, anti_paths=True, mo_match=True):
+        ''' 
+        Paramètres
+        ==========
+        M: int
+        nombre d'intervalles temporels de discrétisation
+        I: int
+        nombre de trajectoires à simuler
+        anti_paths: boolean
+        usage de variables antithétiques
+        mo_match: boolean
+        usage de l'association des moments
+        '''
+        if anti_paths is True:
+            sn = npr.standard_normal((M + 1, int(I / 2)))
+            sn = np.concatenate((sn, -sn), axis=1)
+        else:
+            sn = npr.standard_normal((M + 1, I))
+        if mo_match is True:
+            sn = (sn - sn.mean()) / sn.std()
+        return sn
+    brownian_motion = np.cumsum(gen_sn(M,I), axis=0)
+    return brownian_motion
 
 
 #Simulation du processus
