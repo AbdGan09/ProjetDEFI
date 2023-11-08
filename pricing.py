5618cd63d27194a2947d6c2d0dd4f9d573ccf4ba
PierreL77 <129327699+PierreL77@users.noreply.github.com>
2023-11-08 19:23:56 +0100

    Update pricing.py

--- pricing.py
+++ pricing.py
@@ -1,6 +1,7 @@
 #developpement de tout ce qui est en lien avec la partie pricing
 #importation library
-
+import numpy as np
+import numpy.random as npr
 from quant import *
 
 #fonction qui prend en param le notionnel N, la date de valo, le taux fixe du swap
@@ -9,8 +10,27 @@ def pricingSwap():
 
 
 #simulation de trajectoire
-def generateurTrajectoire():
-    pass
+def gen_sn(M, I, anti_paths=True, mo_match=True):
+    ''' 
+    Paramètres
+    ==========
+    M: int
+    nombre d'intervalles temporels de discrétisation
+    I: int
+    nombre de trajectoires à simuler
+    anti_paths: boolean
+    usage de variables antithétiques
+    mo_match: boolean
+    usage de l'association des moments
+    '''
+    if anti_paths is True:
+        sn = npr.standard_normal((M + 1, int(I / 2)))
+        sn = np.concatenate((sn, -sn), axis=1)
+    else:
+        sn = npr.standard_normal((M + 1, I))
+    if mo_match is True:
+        sn = (sn - sn.mean()) / sn.std()
+    return sn
 
 
 #Simulation du processus
