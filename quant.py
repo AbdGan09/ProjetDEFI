21c565eee7447cc6a1690181fe25ca091942eb92
mayssa99 <mayssamahmoud.prepa@gmail.com>
2023-11-11 16:06:17 +0100

    Changes

--- quant.py
+++ quant.py
@@ -19,16 +19,8 @@ class MarketZeroCoupon:
 
     @staticmethod
     def getmarketZeroCouponInstFwdCurve(T, Betha0 = 2.61, Betha1 = -1.33, lambdas = 0.17):
-        f = lambda x: -log(getMarketZeroCouponCurve(x, Betha0 = 2.61, Betha1 = -1.33, lambdas = 0.17))
+        f = lambda x: -np.log(getMarketZeroCouponCurve(x, Betha0 = 2.61, Betha1 = -1.33, lambdas = 0.17))
         return scipy.misc.derivative(f, T)
-
-#comme discuté j'ai juste écrire les classes les fonctions à l'intérieur on se les repartis
-
-
-#si quelqu'un juge necessaire de faire des function suplémentaire il peux le faire l'objectif étant de mieu segmenté notre code pour le rendre plus lisible
-
-
-
 
 #MarketZeroCouponCurve
 def marketZeroCouponCurve():
